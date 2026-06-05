40fd78989e787b20c9b016785524271a316757e5
rdad123 <99907438+rdad123@users.noreply.github.com>
2026-06-05 19:39:19 +0100

    changed the python ai to output some more game data to see if the python listener recieved said data

--- Mage.Server.Plugins/Mage.Player.AI/src/main/java/mage/player/ai/PythonAIPlayer.java
+++ Mage.Server.Plugins/Mage.Player.AI/src/main/java/mage/player/ai/PythonAIPlayer.java
@@ -34,20 +34,67 @@ public class PythonAIPlayer extends ComputerPlayer {
             mage.players.Player myPlayer = game.getPlayer(this.playerId);
 
             if (myPlayer != null) {
-                int life = myPlayer.getLife();
-                int handSize = myPlayer.getHand().size();
+                int myLife = myPlayer.getLife();
                 int turnNumber = game.getTurnNum();
 
+                // 1. Get Opponents
+                StringBuilder opponentsJson = new StringBuilder("[");
+                boolean firstOpponent = true;
+                for (java.util.UUID currentId : game.getPlayerList()) {
+                    if (!currentId.equals(this.playerId)) {
+                        mage.players.Player opponent = game.getPlayer(currentId);
+                        if (opponent != null) {
+                            if (!firstOpponent) opponentsJson.append(", ");
+                            opponentsJson.append(String.format(
+                                    "{\"name\": \"%s\", \"life\": %d, \"hand_size\": %d}",
+                                    opponent.getName(), opponent.getLife(), opponent.getHand().size()
+                            ));
+                            firstOpponent = false;
+                        }
+                    }
+                }
+                opponentsJson.append("]");
+
+                // 2. Get My Hand (Need IDs to cast them later!)
+                StringBuilder handJson = new StringBuilder("[");
+                boolean firstCard = true;
+                for (mage.cards.Card card : myPlayer.getHand().getCards(game)) {
+                    if (!firstCard) handJson.append(", ");
+                    // We must escape quotes in card names just in case!
+                    String safeName = card.getName().replace("\"", "\\\"");
+                    handJson.append(String.format(
+                            "{\"id\": \"%s\", \"name\": \"%s\"}",
+                            card.getId().toString(), safeName
+                    ));
+                    firstCard = false;
+                }
+                handJson.append("]");
+
+                // 3. Get My Battlefield (Lands, Creatures, etc.)
+                StringBuilder fieldJson = new StringBuilder("[");
+                boolean firstPerm = true;
+                for (mage.game.permanent.Permanent perm : game.getBattlefield().getAllActivePermanents(this.playerId)) {
+                    if (!firstPerm) fieldJson.append(", ");
+                    String safeName = perm.getName().replace("\"", "\\\"");
+                    fieldJson.append(String.format(
+                            "{\"id\": \"%s\", \"name\": \"%s\", \"tapped\": %b, \"power\": %d, \"toughness\": %d}",
+                            perm.getId().toString(), safeName, perm.isTapped(), perm.getPower().getValue(), perm.getToughness().getValue()
+                    ));
+                    firstPerm = false;
+                }
+                fieldJson.append("]");
+
+                // 4. Combine into final massive JSON string
                 String gameStateJson = String.format(
-                        "{\"life\": %d, \"hand_size\": %d, \"turn\": %d}",
-                        life, handSize, turnNumber
+                        "{\"turn\": %d, \"my_life\": %d, \"opponents\": %s, \"my_hand\": %s, \"my_battlefield\": %s}",
+                        turnNumber, myLife, opponentsJson.toString(), handJson.toString(), fieldJson.toString()
                 );
 
+                // 5. Send it
                 out.println(gameStateJson);
             }
 
             String response = in.readLine();
-
             socket.close();
 
         } catch (Exception e) {
